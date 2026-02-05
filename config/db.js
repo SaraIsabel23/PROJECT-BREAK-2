fb17d47969337788e5f0574d50699cdc9a7aa25c
Sara Isabel del Sastre <chinchimilla@gmail.com>
2026-02-05 13:18:58 +0100

    en proceso

--- config/db.js
+++ config/db.js
@@ -1,1 +1,15 @@
 // db.js configuracion de la base de datos.
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const dbConnection = async() => {
+    try{
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('Base de datos conectada con éxito');
+    }catch(error){
+        console.error(error);
+        throw new Error('Error al iniciar la base de datos')
+    }
+};
+
+module.exports = dbConnection;
--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // index.js: Es el punto de entrada de la aplicación, donde se arranca el servidor y se conecta todo.
+
+const express      = require('express');
+const dbConnection = require('./config/db');
+const app          = express();
+const PORT         = process.env.PORT || 3000;
+
+require('dotenv').config();
+
+app.get('/', (req, res) => {
+    res.send("Hola Mundo")
+});
+
+dbConnection()
+
+app.listen(PORT, () => console.log(`Server started on port http://localhost:${PORT}`));
+console.log("URI:", process.env.MONGO_URI);
